161ae48c4a62b04c3693ca4ab3d2d4c7836a1681
Eli Dykes <elidykes@unc.edu>
2025-03-04 23:15:35 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,7 +1,7 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
-export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
-export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s home appliance AI assistant.`;
+export const DEFAULT_RESPONSE_MESSAGE: string = `It appears that your genie needs to take a break. Check back later to get the answer to that question!`;
+export const WORD_CUTOFF: number = 5000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
