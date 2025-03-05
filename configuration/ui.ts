ee46dc89008a22b4224bcf756bad7940ca98764d
Eli Dykes <elidykes@unc.edu>
2025-03-04 23:37:59 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `SPARK`;
+export const CHAT_HEADER: string = `SWEATSYNC`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[What would you like to know today?]`;
-export const CLEAR_BUTTON_TEXT: string = `CLEAR CHAT HISTORY`;
+export const FOOTER_MESSAGE: string = `[What would you like to learn today?]`;
+export const CLEAR_BUTTON_TEXT: string = `Delete Chat`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI fitness assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
