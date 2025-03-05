ad636df0fceb6f13f1016f155c0945143b0c7b1c
Eli Dykes <elidykes@unc.edu>
2025-03-04 23:26:15 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `HOMEGENIE`;
+export const CHAT_HEADER: string = `SPARK`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `[What would you like to know today?]`;
 export const CLEAR_BUTTON_TEXT: string = `CLEAR CHAT HISTORY`;
