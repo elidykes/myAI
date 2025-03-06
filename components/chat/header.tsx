51a081fd220300b6097c84433e2477f75feaa63c
Eli Dykes <elidykes@unc.edu>
2025-03-06 13:16:13 -0500

    Update header.tsx

--- components/chat/header.tsx
+++ components/chat/header.tsx
@@ -6,7 +6,7 @@ import { AI_NAME } from "@/configuration/identity";
 
 export const AILogo = () => (
   <div className="w-12 h-12 relative">
-    <Image src="/fitness_chatbot_logo_transparent.png" alt={AI_NAME} width={48} height={48} />
+    <Image src="/fitness_chatbot_logo_transparent-removebg-preview.png" alt={AI_NAME} width={48} height={48} />
     <div className="w-2 h-2 rounded-full bg-green-500 absolute -bottom-0.5 -right-0.5"></div>
   </div>
 );
