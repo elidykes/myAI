6aa596f0cce7afa73f38944de21682524dffc872
Eli Dykes <elidykes@unc.edu>
2025-03-06 13:27:06 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI fitness assistant.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI fitness assistant. How can I assist you with your fitness goals today?`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `It appears that even I have worked out a little too hard. Check back later to get a better response!`;
 export const WORD_CUTOFF: number = 5000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
