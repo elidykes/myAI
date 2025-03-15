961e7beaf72701dada76d98062807ec83fb2ded4
Eli Dykes <elidykes@unc.edu>
2025-03-15 03:42:46 -0400

    Update prompts.ts

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -52,7 +52,7 @@ Use the following excerpts from ${OWNER_NAME} to answer the user's question. If 
 Excerpts from ${OWNER_NAME}:
 ${context}
 
-If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "While not directly discussed in the documents that ${OWNER_NAME} provided me with, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
+If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "I believe that I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
 
 Respond with the following tone: ${AI_TONE}
 
@@ -60,17 +60,6 @@ Now respond to the user's message:
 `;
 }
 
-export function RESPOND_TO_QUESTION_BACKUP_SYSTEM_PROMPT() {
-  return `
-${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
-
-You couldn't perform a proper search for the user's question, but still answer the question starting with "While I couldn't perform a search due to an error, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
-
-Respond with the following tone: ${AI_TONE}
-
-Now respond to the user's message:
-`;
-}
 
 export function HYDE_PROMPT(chat: Chat) {
   const mostRecentMessages = chat.messages.slice(-3);
